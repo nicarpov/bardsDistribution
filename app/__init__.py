fde14bdedb4611ec28d8fd59b6b12db015e4fbe8
Nikita Arapov <nicarpov@gmail.com>
2023-12-03 15:38:29 +0300

    User login func added

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import  Migrate
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,5 +11,7 @@ app.config.from_object(Config)
 bootstrap = Bootstrap5(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from app import app
-from flask import render_template, flash, redirect
+from app import app, db
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm, RegisterForm
+from app.models import User
+from flask_login import current_user, login_user
+import sqlalchemy as sa
 
 
 @app.route('/')
@@ -21,9 +24,17 @@ def signup():
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
+    if current_user.is_authenticated():
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested by user {}, remember_me {}'
-              .format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            redirect(url_for('signin'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('signin.html', title='Sign In', form=form)
